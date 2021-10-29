5d23248ec2b9eb1d4c9f366338f67b30f8399931
James Carlson <jxxcarlson@gmail.com>
2021-10-29 13:38:40 -0400

    Edit migration

--- src/Evergreen/V48/Types.elm
+++ src/Evergreen/V48/Types.elm
@@ -126,7 +126,7 @@ type FrontendMsg
     | RunSpecial
     | ExportJson
     | JsonRequested
-    | JsonSelected File
+    | JsonSelected File.File
     | JsonLoaded String
     | SignIn
     | SignOut
