d2cdfd50ea1b4f1ea3e85a7e6fa0601d70f95ba9
James Carlson <jxxcarlson@Jamess-MacBook-Pro.local>
2021-11-02 11:30:35 -0400

    Try to make init functon for ace-element.js

--- elm-pkg-js-includes.js
+++ elm-pkg-js-includes.js
@@ -10,7 +10,7 @@ exports.init = async function init(app) {
   // This file will not run in Local development, an equivalent to this is
   // automatically generated in Local Development for every file in elm-pkg-js/
   elm_katex.init(app)
-
+  ace_element.init(app)
 
 
 }
