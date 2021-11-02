6e9ef801a7ef89a335b0db66ff694494cacc07b0
James Carlson <jxxcarlson@gmail.com>
2021-11-02 19:30:47 -0400

    correct previous migration

--- src/Evergreen/V73/Types.elm
+++ src/Evergreen/V73/Types.elm
@@ -141,7 +141,7 @@ type FrontendMsg
     | RunSpecial
     | ExportJson
     | JsonRequested
-    | JsonSelected File
+    | JsonSelected File.File
     | JsonLoaded String
     | SignIn
     | SignOut
--- src/Evergreen/V74/Types.elm
+++ src/Evergreen/V74/Types.elm
@@ -142,7 +142,7 @@ type FrontendMsg
     | RunSpecial
     | ExportJson
     | JsonRequested
-    | JsonSelected File.File
+    | JsonSelected File
     | JsonLoaded String
     | SignIn
     | SignOut
