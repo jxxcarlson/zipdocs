4e29de6df034ab91d7e27939e4e00e88897bf2d6
James Carlson <jxxcarlson@gmail.com>
2021-11-04 18:48:28 -0400

    Add File.File to V78 migration

--- src/Evergreen/V78/Types.elm
+++ src/Evergreen/V78/Types.elm
@@ -145,7 +145,7 @@ type FrontendMsg
     | RunSpecial
     | ExportJson
     | JsonRequested
-    | JsonSelected File
+    | JsonSelected File.File
     | JsonLoaded String
     | SignIn
     | SignOut
