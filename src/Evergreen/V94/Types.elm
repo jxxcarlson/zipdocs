550058953232327a75de27057c57068287281feb
James Carlson <jxxcarlson@gmail.com>
2021-11-10 12:10:45 -0500

    Preparing for v94

--- src/Evergreen/V94/Types.elm
+++ src/Evergreen/V94/Types.elm
@@ -145,7 +145,7 @@ type FrontendMsg
     | RunSpecial
     | ExportJson
     | JsonRequested
-    | JsonSelected File.File
+    | JsonSelected File
     | JsonLoaded String
     | SignIn
     | SignOut
